47fa9f1858691dd333bf4214ccba0ad953a215f3
Laurent Binder <laurentbinder@gmail.com>
2025-05-23 13:04:20 +0200

    redirectUrl if orgId or userId

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,31 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
 
 const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/']);
 
+const handleRedirectAfterAuth = (auth, req) => {
+  const { userId, orgId, isPublicRoute } = auth();
+
+  if (!userId) return;
+
+  // If user has no organization, redirect to /select-org
+  if (userId && isPublicRoute) {
+    let path = '/select-org';
+    // If the user has an organization, redirect to /organization/:id
+    if (orgId) {
+      path = `/organization/${orgId}`;
+    }
+
+    const redirectUrl = new URL(path, req.url);
+    return NextResponse.redirect(redirectUrl);
+  }
+};
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
     await auth.protect();
   }
+
+  return handleRedirectAfterAuth(auth, req) ?? NextResponse.next();
 });
 
 export const config = {
