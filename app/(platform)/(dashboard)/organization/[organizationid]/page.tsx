64d5cf78cbb9fa3a2ad8144d3d4393c291d5d8a6
Laurent Binder <laurentbinder@gmail.com>
2025-06-18 18:14:00 +0200

    starting Board Schema

--- app/(platform)/(dashboard)/organization/[organizationid]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationid]/page.tsx
@@ -1,7 +1,21 @@
+'use client';
+
 const OrganizationIdPage = () => {
   // const user = await currentUser();
   // const { userId } = useAuth();
   // console.log('🚀 ~ OrganizationPage ~ userId:', userId?.sessionId);
-  return <div>Organization page!</div>;
+  return (
+    <div>
+      <form>
+        <input
+          id='title'
+          name='title'
+          required
+          placeholder='Enter a board title'
+          className='border border-gray-300 rounded-md p-2 w-full'
+        />
+      </form>
+    </div>
+  );
 };
 export default OrganizationIdPage;
